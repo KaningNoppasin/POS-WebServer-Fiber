c976ddf4e02b43f048dbcf3a9d3c5bac2a2c2057
NN <95127494+KaningNoppasin@users.noreply.github.com>
2025-03-13 20:52:33 +0700

    style: logger

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -3,10 +3,12 @@ package database
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConnectDB() *gorm.DB {
@@ -14,8 +16,22 @@ func ConnectDB() *gorm.DB {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,          // Don't include params in the SQL log
+			Colorful:                  true,         // Enable color
+		},
+	)
+
 	connStr := os.Getenv("DATABASE_URL")
-	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		panic(err)
 	}
